eab85e9c553c7db97aa0850ad145e40d6a92b0c0
Veysel KUŞ <kusveysel00@gmail.com>
2026-08-10 13:44:25 +0300

    10/08/2026

--- 18-MathModule.py
+++ 18-MathModule.py
@@ -52,3 +52,33 @@ print("math.exp(1) =>",math.exp(1)) #* e üzeri 1.parametre(1)
 print("math.expm1(1) =>",math.expm1(1)) #* e üzeri 1.parametre(1) 
 #* exp() ve expm1() arsındaki fark expm1() hassas ölçümler için daha iyidir
 print("math.exp2(1) =>",math.exp2(1)) #* 2 üzeri 1.parametre(1)
+
+#?
+print()
+print("math.fabs(-7) =>",math.fabs(-7)) #* mutlak değer alır
+print("math.factorial(7) =>",math.factorial(7)) #* faktöriyel alır
+print("math.fmod(19,4) =>",math.fmod(19,4)) #* 1.parametrenin 2.parametreye bölümünden kalanı verir
+print("math.frexp(19) =>",math.frexp(19)) #* 1.parametreyi 2^x * y şeklinde verir
+print("math.fsum([1,2,3,4,5]) =>",math.fsum([1,2,3,4,5])) #* float tipinde toplam alır
+print("math.gamma(7) =>",math.gamma(7)) #* gamma fonksiyonu
+print("math.lgamma(7) =>",math.lgamma(7)) #* gamma fonksiyonunun logaritmasını alır
+print("math.prod([19,4]) =>",math.prod([19,4])) #* içindeki sayıların çarpımını verir
+
+#?
+print()
+print("math.gcd(19,4) =>",math.gcd(19,4)) #* 1.parametre ile 2.parametrenin en büyük ortak bölenini verir
+print("math.hypot(3,4) =>",math.hypot(3,4)) #* 2 parametreyi hipotenüs olarak kabul edip hipotenüsü verir
+print("math.isclose(0.1+0.2,0.3) =>",math.isclose(0.1+0.2,0.3)) #* 2 parametreyi birbirine yakın mı diye kontrol eder
+print("math.isfinite(7) =>",math.isfinite(7)) #* 1.parametre sonsuz mu diye kontrol eder
+print("math.isinf(7) =>",math.isinf(7)) #* 1.parametre sonsuz mu diye kontrol eder
+print("math.isnan(7) =>",math.isnan(7)) #* 1.parametre nan mı diye kontrol eder
+print("math.frexp(10) =>",math.frexp(10)) #* 1.parametreni 2^x * y şeklinde verir
+print("math.ldexp(10,2) =>",math.ldexp(10,2)) #* 1.parametreyi 2 üzeri 2.parametre ile çarpar
+print("math.log(16,2) =>",math.log(16,2)) #* 1.parametrenin doğal logaritmasını alır
+print("math.log10(100) =>",math.log10(100)) #* 1.parametrenin 10 tabanında logaritmasını alır
+print("math.log1p(100) =>",math.log1p(100)) #* 1.parametrenin 1 fazlasının doğal logaritmasını alır
+print("math.log2(100) =>",math.log2(100)) #* 1.parametrenin 2 tabanında logaritmasını alır
+print("math.perm(10,5) =>",math.perm(10,5)) #* 1.parametrenin 2.parametreye göre permütasyonunu alır
+print("math.pow(10,2) =>",math.pow(10,2)) #* 1.parametrenin 2.parametreye göre kuvvetini alır
+print("math.modf(10.5) =>",math.modf(10.5)) #* 1.parametrenin ondalıklı ve tam kısmını verir
+
---
+++ 19-CmathModule.py
@@ -0,0 +1,16 @@
+import cmath
+#? Karmaşık sayılar üzerinde matematiksel işlemler
+
+print("-----Sabitler-----")
+print("cmath.e =>",cmath.e)
+print("cmath.inf =>",cmath.inf)
+print("-cmath.inf =>",-cmath.inf)
+print("cmath.infj =>",cmath.infj)
+print("cmath.nan =>",cmath.nan)
+print("cmath.nanj =>",cmath.nanj)
+print("cmath.tau =>",cmath.tau)
+
+print("cmath.sqrt(-1) =>",cmath.sqrt(-1)) #* karakökü alır
+print("cmath.sqrt(4+3j) =>",cmath.sqrt(4+3j)) #* karakökü alır
+
+#? 
